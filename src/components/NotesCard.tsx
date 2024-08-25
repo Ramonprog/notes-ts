51cbad411998b992767317c0c3a8daba712aba37
Ramonprog <programador.ramon@gmail.com>
2024-08-25 11:29:41 -0300

    feat: tornando card na altura do text area

--- src/components/NotesCard.tsx
+++ src/components/NotesCard.tsx
@@ -1,13 +1,27 @@
 import { Trash2 } from "lucide-react";
 import { FakeDataItem } from "../types";
+import { useEffect, useRef } from "react";
 
 export function NotesCard({ note }: { note: FakeDataItem }) {
 
   const colors = note.colors
-  // const position = note.position
+  const position = note.position
+
+  const textAreaRef = useRef<HTMLTextAreaElement>(null)
+
+  useEffect(() => {
+    autoGrow(textAreaRef.current)
+  }, [])
+
+  function autoGrow(textArea: HTMLTextAreaElement | null) {
+    if (textArea) {
+      textArea.style.height = "auto"; // Reseta a altura
+      textArea.style.height = textArea.scrollHeight + "px"; // Define a nova altura
+    }
+  }
 
   return (
-    <div className="card" style={{ backgroundColor: colors.colorBody }}>
+    <div className="card" style={{ backgroundColor: colors.colorBody, left: position.x, top: position.y }}>
       <div
         className="card-header"
         style={{ backgroundColor: colors.colorHeader }}
@@ -17,8 +31,10 @@ export function NotesCard({ note }: { note: FakeDataItem }) {
 
       <div className="card-body">
         <textarea
+          ref={textAreaRef}
           style={{ color: colors.colorText }}
           defaultValue={note.body}
+          onInput={() => autoGrow(textAreaRef.current)}
         >
 
         </textarea>
